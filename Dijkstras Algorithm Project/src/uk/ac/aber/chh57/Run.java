6ffaf0617531f24cba6f50b39a4633977f4636bf
sparkhead95 <christianosholland@gmail.com>
2015-04-11 17:39:28 +0100

    On the right track. Class has been implemented

--- Dijkstras Algorithm Project/src/uk/ac/aber/chh57/Run.java
+++ Dijkstras Algorithm Project/src/uk/ac/aber/chh57/Run.java
@@ -1,5 +1,21 @@
 package uk.ac.aber.chh57;
 
-public class Run {
+import java.awt.Point;
+import java.util.LinkedList;
+
+import cs21120.assignment2.FloatImage;
+import cs21120.assignment2.ISnapper;
+
+public class Run implements ISnapper {
+
+	public LinkedList<Point> getPath(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setSeed(int arg0, int arg1, FloatImage[] arg2) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
