1adc3f1f2a6aaa3d2dae0a9cbf45d7ae7cb22807
sparkhead95 <christianosholland@gmail.com>
2015-04-12 14:54:52 +0100

    Added Edge class - implements comparable

---
+++ Dijkstras Algorithm Project/src/uk/ac/aber/chh57/Edge.java
@@ -0,0 +1,12 @@
+package uk.ac.aber.chh57;
+
+public class Edge implements Comparable<Edge> {
+
+	String type = ""; // can be start point, end point or weight
+	
+	public int compareTo(Edge otherEdge) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
